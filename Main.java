24eb535117fe202ae939b9da170256eeafd944e9
kenta-matsuzaki <kenta6910@gmail.com>
2020-06-15 14:22:57 +0900

    Main.java,Main.classのみcommit

--- Main.java
+++ Main.java
@@ -4,10 +4,23 @@ public class Main {
     s.name = "炎の剣";
     s.damage = 10;
     
-    Hero h = new Hero();
-    h.name = "健太";
-    h.hp = 383;
-    h.sword = s;
-    System.out.println(h.name + "の現在の武器は" + h.sword.name);
+    Hero h1 = new Hero();
+    h1.name = "健太";
+    h1.hp = 383;
+    h1.sword = s;
+
+    Hero h2 = new Hero();
+    h2.name = "松崎";
+    h2.hp = 182;
+
+    Wizard w = new Wizard();
+    w.name = "スガワラ";
+    w.hp = 88;
+    
+    w.heal(h1);
+    w.heal(h2);
+    w.heal(h2);
+    
+    System.out.println(h1.name + "の現在の武器は" + h1.sword.name);
   }
 }
